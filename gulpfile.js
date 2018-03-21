955d6c35ae5de949767c60efd50b1f58271ad82d
Erik Christianson <internet.erik@gmail.com>
2018-03-21 15:03:40 -0500

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+/*
+ * Create variables for our project paths so we can change in one place
+ */
+var paths = {
+  'sass'     : './scss/**/*.scss',
+  'css'      : './css/',
+};
+
+/**
+ * Builds the scss files
+ */
+gulp.task('sass', function(){
+  return gulp.src(paths.sass)
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('default', ['sass'], function() {});
+
+gulp.task('watch', function() {
+    gulp.start('sass');
+    gulp.watch(paths.sass, ['sass']);
+});
